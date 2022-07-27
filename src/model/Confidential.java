f838d2f4710e4c11b24dbb670f0fc1143ae691e5
Santiago José Barbosa Rivas <102764356+BARBOSA191919@users.noreply.github.com>
2022-07-27 16:08:13 -0500

    Add files via upload

--- src/model/Confidential.java
+++ src/model/Confidential.java
@@ -1,29 +1,157 @@
-
 package model;
+
 
 //SANTIAGO JOSÉ BARBOSA RIVAS        CODIGO : 20212200802
 //JONATHAN FELIPE SANCHÉZ MATHEUS    CÓDIGO : 20212200869
 
-public class Confidential extends ConfidentialPerson  {
-    
+public class Confidential extends Person {
+    private Long id;
+    private String fullName;
+    private String password;
+    private String phone;
+    private String email;
+    private String date;
+    private String employeessalary;
+    private String taxreturns;
+    private String insurancerecords;
+    private String bankcards;
+    private String financialinstitutions;
+    private String country;
+    private String city;
+    private String source;
     private boolean state;
     private String type;
     private String notes;
+    private Organization org;
     
-    public Confidential (boolean state, String type, String notes,Long id, String fullName, String employeessalary, String phone, String email ,String taxreturns,String date,String insurancerecords, String bankcards,String financialinstitutions, String password) {
-        super(id, fullName,password, phone, email,date ,employeessalary, taxreturns,insurancerecords,bankcards,financialinstitutions);
+    public Confidential (boolean state, String type, String notes,Long id, String fullName, String employeessalary, String phone, String email ,String taxreturns,String date,String insurancerecords, String bankcards,String financialinstitutions, String password,String country, String city , String source) {
+     super(id, fullName,employeessalary , phone, email);
+        this.id = id;
+        this.fullName = fullName;
+        this.phone = phone;
+        this.email = email;
+        this.date = date;
+        this.employeessalary = employeessalary;
+        this.taxreturns=taxreturns;
+        this.insurancerecords = insurancerecords;
+        this.bankcards = bankcards;
+        this.financialinstitutions = financialinstitutions;
+        this.password=password;
+        this.country=country;
+        this.city=city;
+        this.source=source;
         this.state = state;
         this.type = type;
         this.notes = notes;
-}
-     public Confidential (boolean state, String type, String notes) {
-        this.state = state;
-        this.type = type;
-        this.notes = notes;
+
  }
-    public Confidential() {
-   
+    public Confidential (boolean state, String type, String notes) {
+        this.state = state;
+        this.type = type;
+        this.notes = notes;
     }
+    public Confidential() {
+        
+    }
+        public Confidential (Long id, String fullName, String employeessalary, String phone, String email ,String taxreturns,String date,String insurancerecords, String bankcards,String financialinstitutions, String password,String country, String city , String source) {
+        super (id, fullName,employeessalary , phone, email );
+                }
+         public Confidential (boolean state, String type, String notes,Long id, Organization org ,String fullName, String employeessalary, String phone, String email ,String taxreturns,String date,String insurancerecords, String bankcards,String financialinstitutions, String password,String country, String city , String source) {
+         super (id , fullName,employeessalary , phone, email);
+        this.state = state;
+        this.type = type;
+        this.notes = notes;
+        this.org = org;
+            }
+        public Confidential(boolean state, String type, String notes, Organization org) {
+        this.state = state;
+        this.type = type;
+        this.notes = notes;
+        this.org = org;
+    }
+
+    
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getEmployeessalary() {
+        return employeessalary;
+    }
+
+    public void setEmployeessalary(String employeessalary) {
+        this.employeessalary = employeessalary;
+    }
+
+    public String getTaxreturns() {
+        return taxreturns;
+    }
+
+    public void setTaxreturns(String taxreturns) {
+        this.taxreturns = taxreturns;
+    }
+
+    public String getInsurancerecords() {
+        return insurancerecords;
+    }
+
+    public void setInsurancerecords(String insurancerecords) {
+        this.insurancerecords = insurancerecords;
+    }
+
+    public String getBankcards() {
+        return bankcards;
+    }
+
+    public void setBankcards(String bankcards) {
+        this.bankcards = bankcards;
+    }
+
+    public String getFinancialinstitutions() {
+        return financialinstitutions;
+    }
+
+    public void setFinancialinstitutions(String financialinstitutions) {
+        this.financialinstitutions = financialinstitutions;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+    
+    
     public boolean isState() {
         return state;
     }
@@ -47,8 +175,21 @@ public class Confidential extends ConfidentialPerson  {
     public void setNotes(String notes) {
         this.notes = notes;
     }
+      public Organization getOrg() {
+        return org;
+    }
+
+    public void setOrg(Organization org) {
+        this.org = org;
+    }
+    
    @Override
     public String toString() {
-        return "Letter{" + "state=" + state + ", type=" + type + ", notes=" + notes + '}'+' ' + super.toString();     
+         return "Confidential{" + "state=" + state + ", type=" + type + ", notes=" + notes + '}'+' ' + super.toString() + '-' + org.toString();
 }
+
 }
+
+
+
+    
--- src/model/Letter.java
+++ src/model/Letter.java
@@ -1,32 +1,85 @@
-
 package model;
 
 //SANTIAGO JOSÉ BARBOSA RIVAS        CODIGO : 20212200802
 //JONATHAN FELIPE SANCHÉZ MATHEUS    CÓDIGO : 20212200869
 
-public class Letter extends lettersperson {
+public class Letter extends Person {
+    private Long id;
+    private String fullNameorbusiness;
+    private String Recipientsname;
+    private String date;
+    private String address;
+    private String affair;
+    private String phone;
+    private String email;
+    private String PostalCode;
+    private String place;
+    private String firm;
+    private String source;
+    private String country;
     private boolean state;
     private String type;
     private String notes;
+    private Organization org;
     
-    public Letter(boolean state, String type, String notes,Long id, String fullNameorbusiness, String address, String phone, String email ,String Recipientsname,String date,String affair,String PostalCode, String place,String firm) {
-        super(id, fullNameorbusiness, Recipientsname, date, address,affair,phone,email,PostalCode,place,firm);
+    
+    public Letter(boolean state, String type, String notes,Long id, String fullNameorbusiness, String address, String phone, String email ,String Recipientsname,String date,String affair,String PostalCode,  String place,String firm,String source, String country) {
+         super(id, fullNameorbusiness,address , phone, email);
+        this.id = id;
+        this.fullNameorbusiness = fullNameorbusiness;
+        this.Recipientsname=Recipientsname;
+        this.date = date;
+        this.address = address;
+        this.affair = affair;
+        this.phone = phone;
+        this.email = email;
+        this.PostalCode = PostalCode;
+        this.place = place;
+        this.firm = firm;
+        this.source = source;
+        this.country = country;
+        this.state = state;
+        this.type = type;
+        this.notes = notes;
+
+    }
+    public Letter (boolean state, String type, String notes) {
         this.state = state;
         this.type = type;
         this.notes = notes;
     }
-
-    public Letter(boolean state, String type, String notes) {
+public Letter() {
+    }
+ public Letter (Long id, String fullNameorbusiness, String address, String phone, String email ,String Recipientsname,String date,String affair,String PostalCode,  String place,String firm,String source, String country) {
+        super (id, fullNameorbusiness,address , phone, email );
+              }
+  public Letter (boolean state, String type, String notes,Long id,Organization org, String fullNameorbusiness, String address, String phone, String email ,String Recipientsname,String date,String affair,String PostalCode,  String place,String firm,String source, String country) {
+         super (id, fullNameorbusiness,address , phone, email);
         this.state = state;
         this.type = type;
         this.notes = notes;
-   }
-    
-    public Letter() {
+        this.org = org;
+            }
+        public Letter(boolean state, String type, String notes, Organization org) {
+        this.state = state;
+        this.type = type;
+        this.notes = notes;
+        this.org = org;
+        }
+        
+    public String getCountry() {
+        return country;
     }
 
-    public Letter(Long id, String fullNameorbusiness, String address, String phone, String email, String Recipientsname, String date, String affair, String PostalCode, String place, String firm) {
-        super(id, fullNameorbusiness, Recipientsname, date, address,affair,phone,email,PostalCode,place,firm);
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getSource() {
+        return source;
+    }
+    public void setSource(String source) {
+        this.source = source;
     }
 
     public boolean isState() {
@@ -44,7 +97,6 @@ public class Letter extends lettersperson {
     public void setType(String type) {
         this.type = type;
     }
-
     public String getNotes() {
         return notes;
     }
@@ -52,10 +104,70 @@ public class Letter extends lettersperson {
     public void setNotes(String notes) {
         this.notes = notes;
     }
-    
-     @Override
-    public String toString() {
-        return "Letter{" + "state=" + state + ", type=" + type + ", notes=" + notes + '}'+' ' + super.toString();
+    public String getFullNameorbusiness() {
+        return fullNameorbusiness;
     }
-            
+
+    public void setFullNameorbusiness(String fullNameorbusiness) {
+        this.fullNameorbusiness = fullNameorbusiness;
+    }
+
+    public String getRecipientsname() {
+        return Recipientsname;
+    }
+
+    public void setRecipientsname(String Recipientsname) {
+        this.Recipientsname = Recipientsname;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getAffair() {
+        return affair;
+    }
+
+    public void setAffair(String affair) {
+        this.affair = affair;
+    }
+    public String getPostalCode() {
+        return PostalCode;
+    }
+
+    public void setPostalCode(String PostalCode) {
+        this.PostalCode = PostalCode;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getFirm() {
+        return firm;
+    }
+
+    public void setFirm(String firm) {
+        this.firm = firm;
+    }
+      public Organization getOrg() {
+        return org;
+    }
+
+    public void setOrg(Organization org) {
+        this.org = org;
+    }
+    @Override
+    public String toString() {
+        return "Person{" + "id=" + id + ", fullNameorbusiness=" + fullNameorbusiness + ", address=" + address + ", phone=" + phone + ", email=" + email + ", Recipientsname=" + Recipientsname + ", date=" + date + ", affair=" + affair + ", PostalCode=" + PostalCode + ", place=" + place +  ", firm=" + firm +", type= "+ type + " , state=" + state + " , source=" + source +", country=" + country +" , notes=" + notes+'}';
+    
+    }    
 }
